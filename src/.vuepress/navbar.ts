53a207024af63c66f271f4fa5e82cb2e6ea9f051
taoeaten <taoeaten@163.com>
2024-09-11 22:15:19 +0800

    目录

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -10,48 +10,48 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  "/demo/",
-  {
-    text: "Blog",
-    icon: "pen-to-square",
-    prefix: "/posts/",
-    children: [
-      {
-        text: "苹果",
-        icon: "pen-to-square",
-        prefix: "apple/",
-        children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
-          { text: "苹果2", icon: "pen-to-square", link: "2" },
-          "3",
-          "4",
-        ],
-      },
-      {
-        text: "香蕉",
-        icon: "pen-to-square",
-        prefix: "banana/",
-        children: [
-          {
-            text: "香蕉 1",
-            icon: "pen-to-square",
-            link: "1",
-          },
-          {
-            text: "香蕉 2",
-            icon: "pen-to-square",
-            link: "2",
-          },
-          "3",
-          "4",
-        ],
-      },
-      { text: "樱桃", icon: "pen-to-square", link: "cherry" },
-      { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
-      "tomato",
-      "strawberry",
-    ],
-  },
+  //"/demo/",
+  // {
+  //   text: "Blog",
+  //   icon: "pen-to-square",
+  //   prefix: "/posts/",
+  //   children: [
+  //     {
+  //       text: "苹果",
+  //       icon: "pen-to-square",
+  //       prefix: "apple/",
+  //       children: [
+  //         { text: "苹果1", icon: "pen-to-square", link: "1" },
+  //         { text: "苹果2", icon: "pen-to-square", link: "2" },
+  //         "3",
+  //         "4",
+  //       ],
+  //     },
+  //     {
+  //       text: "香蕉",
+  //       icon: "pen-to-square",
+  //       prefix: "banana/",
+  //       children: [
+  //         {
+  //           text: "香蕉 1",
+  //           icon: "pen-to-square",
+  //           link: "1",
+  //         },
+  //         {
+  //           text: "香蕉 2",
+  //           icon: "pen-to-square",
+  //           link: "2",
+  //         },
+  //         "3",
+  //         "4",
+  //       ],
+  //     },
+  //     { text: "樱桃", icon: "pen-to-square", link: "cherry" },
+  //     { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
+  //     "tomato",
+  //     "strawberry",
+  //   ],
+  // },
   {
     text: "Tamm",
     icon: "pen-to-square",
@@ -61,8 +61,24 @@ export default navbar([
     ],
   },
   {
-    text: "V2 文档",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
+    text: "Edes",
+    icon: "pen-to-square",
+    prefix: "/Edes/",
+    children: [
+      { text: "草莓", icon: "pen-to-square", link: "strawberry" },
+    ],
+  },
+  {
+    text: "Marni",
+    icon: "pen-to-square", 
+    prefix: "/Marni/",
+    children: [
+      { text: "樱桃", icon: "pen-to-square", link: "cherry" },
+    ],
+  },
+  {
+    text: "关于我",
+    icon: "info-circle",
+    link: "/intro",
   },
 ]);
